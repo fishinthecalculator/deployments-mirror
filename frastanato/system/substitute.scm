ab557d79938c3c8f7da2e0ec04187c7e31d3cb0a
Giacomo Leidi <goodoldpaul@autistici.org>
2022-12-24 01:40:56 +0100

    frastamato: add substitute

---
+++ frastanato/system/substitute.scm
@@ -0,0 +1,11 @@
+(define-module (frastanato system substitute)
+  #:use-module (gnu)
+  #:use-module (gnu system)
+  #:use-module (small-guix services substitute))
+
+(define %frastanato-authorized-keys
+  (append %small-guix-authorized-guix-keys
+          (list (file-append
+                 (local-file "../.." "guix-deployments-root-dir"
+                             #:recursive? #t)
+                 "/keys/guix/prematurata.pub"))))
