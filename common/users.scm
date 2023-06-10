28b77f36f17e0396313e81302e86af8b3f15e30d
Giacomo Leidi <goodoldpaul@autistici.org>
2023-06-10 20:41:22 +0200

    common:  Fix imports.

--- common/users.scm
+++ common/users.scm
@@ -1,22 +1,5 @@
 (define-module (common users)
-  #:use-module (gnu)
-  #:use-module (gnu packages audio)          ;for bluez-alsa
-  #:use-module (gnu packages linux)          ;for bluez
-  #:use-module (gnu packages networking)     ;for blueman
-  #:use-module (gnu services dbus)           ;for dbus-root-service-type
-  #:use-module (gnu services desktop)        ;for gnome-service-type
-  #:use-module (gnu services mcron)          ;for mcron-service-type
-  #:use-module (gnu services networking)     ;for tor-service-type
-  #:use-module (gnu services ssh)            ;for ssh-service-type
-  #:use-module (gnu services virtualization) ;for qemu-binfmt-service-type
-  #:use-module (gnu services xorg)           ;for set-xorg-configuration
-  #:use-module (nongnu packages linux)
-  #:use-module (nongnu system linux-initrd)
-  #:use-module (small-guix services mcron)
-  #:use-module (small-guix services desktop)
-  #:use-module (small-guix system desktop)
-  #:use-module (small-guix system input)
-  #:use-module (commons unattended-upgrades))
+  #:use-module (gnu))
 
 (define-public paul-user
   (user-account
