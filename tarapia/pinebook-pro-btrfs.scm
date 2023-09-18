cf10534b3451fc2eea3767de534d9e680cce8a34
Giacomo Leidi <goodoldpaul@autistici.org>
2023-09-18 22:35:56 +0200

    Update tarapia.

--- tarapia/pinebook-pro-btrfs.scm
+++ tarapia/pinebook-pro-btrfs.scm
@@ -3,6 +3,10 @@
 (use-modules (gnu system images pinebook-pro)
 	     (gnu system file-systems))
 
+(use-modules (gnu) (gnu bootloader u-boot))
+(use-service-modules avahi networking ssh)
+(use-package-modules admin bootloaders certs firmware linux ssh)
+
 (operating-system
   (inherit pinebook-pro-barebones-os)
   (file-systems (cons (file-system
