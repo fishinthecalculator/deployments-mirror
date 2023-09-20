d088230fbb00a89e3c020f71d23038399e488b6a
Giacomo Leidi <goodoldpaul@autistici.org>
2023-09-20 21:28:26 +0200

    tarapia: Update fdisk script.

--- tarapia/init.sh
+++ tarapia/init.sh
@@ -27,13 +27,15 @@ part="${dev}p1"
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | sudo fdisk "${dev}"
   g # clear the in memory partition table
   n # new partition
-  p # primary partition
   1 # partition number 1
     # default - start at beginning of disk
     # default, extend partition to end of disk
-  a # make a partition bootable
-  1 # bootable partition is partition 1
+  Y # confirm removal of ext4 signature
+  x # enable expert mode
+  A # make a partition bootable
   p # print the in-memory partition table
+  v # verify the in-memory partition table
+  r # exit Expert mode
   w # write the partition table
   q # and we're done
 EOF
