d1c3411c23dd17e9fbac14fc4e5fcf4a0fcc6dc9
Giacomo Leidi <goodoldpaul@autistici.org>
2023-09-22 21:05:26 +0200

    Update nellone.

--- nellone/system/config.scm
+++ nellone/system/config.scm
@@ -15,43 +15,40 @@
   (list (local-file "../../keys/guix/frastanato.pub")
         (local-file "../../keys/guix/prematurata.pub")))
 
-(define public-host
-  "nellone.fishinthecalculator.me")
-
 (define-public nellone-system
   (guix-nas-system->operating-system
    (guix-nas-system
-     (admin-email "goodoldpaul@autistici.org")
-     (public-host public-host)
-     (enable-https? #t)
-     (host-name "virtual-nellone")
-     ;; The list of user accounts ('root' is implicit).
-     (users (list (user-account
-                   (inherit paul-user)
-                   (supplementary-groups '("wheel" "netdev" "audio" "video" "docker")))))
+    (admin-email "goodoldpaul@autistici.org")
+    (public-host "nellone.fishinthecalculator.me")
+    (enable-https? #t)
+    (host-name "virtual-nellone")
+    ;; The list of user accounts ('root' is implicit).
+    (users (list (user-account
+                  (inherit paul-user)
+                  (supplementary-groups '("wheel" "netdev" "audio" "video" "docker")))))
 
-     (admin-users (list (user-account-name paul-user)))
+    (admin-users (list (user-account-name paul-user)))
 
-     (guix-keys authorized-guix-keys)
+    (guix-keys authorized-guix-keys)
 
-     (ssh-keys authorized-ssh-keys)
+    (ssh-keys authorized-ssh-keys)
 
-     ;; Hardware dependent settings.
-     (bootloader (bootloader-configuration
-                   (bootloader grub-bootloader)
-                   (targets (list "/dev/vda"))
-                   (keyboard-layout (keyboard-layout "us"))))
+    ;; Hardware dependent settings.
+    (bootloader (bootloader-configuration
+                 (bootloader grub-bootloader)
+                 (targets (list "/dev/vda"))
+                 (keyboard-layout (keyboard-layout "us"))))
 
-     (swap-devices (list (swap-space
-                           (target (uuid
-                                    "ba1b7761-8b81-4b71-8a1a-ebdcd0fbcdf6")))))
+    (swap-devices (list (swap-space
+                         (target (uuid
+                                  "ba1b7761-8b81-4b71-8a1a-ebdcd0fbcdf6")))))
 
-     ;; The list of file systems that get "mounted".  The unique
-     ;; file system identifiers there ("UUIDs") can be obtained
-     ;; by running 'blkid' in a terminal.
-     (file-systems (cons* (file-system
-                            (mount-point "/")
-                            (device (uuid
-                                     "f0890300-9702-428f-93ed-a7058a33b2ab"
-                                     'ext4))
-                            (type "ext4")) %base-file-systems)))))
+    ;; The list of file systems that get "mounted".  The unique
+    ;; file system identifiers there ("UUIDs") can be obtained
+    ;; by running 'blkid' in a terminal.
+    (file-systems (cons* (file-system
+                           (mount-point "/")
+                           (device (uuid
+                                    "f0890300-9702-428f-93ed-a7058a33b2ab"
+                                    'ext4))
+                           (type "ext4")) %base-file-systems)))))
