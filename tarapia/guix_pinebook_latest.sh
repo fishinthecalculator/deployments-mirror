5f2e2523958152ffefe4cf975c8329a8bfb1e49f
Giacomo Leidi <goodoldpaul@autistici.org>
2023-09-23 20:14:52 +0200

    tarapia: Update guix_flash_latest.sh.

--- tarapia/guix_pinebook_latest.sh
+++ tarapia/guix_pinebook_latest.sh
@@ -1,12 +1,13 @@
 set -ex
 
-guix_commit="17d9a91e6b8bdd7709b536d34a1f2ef3fcff3b9d"
+here="$(dirname "$0")"
 dev="/dev/nvme0n1"
 part="${dev}p1"
-image="$(guix time-machine --commit=$guix_commit -- system image --image-type=pinebook-pro-raw -e '(@ (gnu system images pinebook-pro) pinebook-pro-barebones-os)' --system=aarch64-linux)"
+image="$(guix time-machine -C "${here}/channels.scm" -- system image --image-type=pinebook-pro-raw -e '(@ (gnu system images pinebook-pro) pinebook-pro-barebones-os)' --system=aarch64-linux)"
 
-sudo dd "if=${image}" "of=${dev}" bs=4M status=progress oflag=sync
-sync
+
+"${here}/pbp-flash.sh" "${image}" "${dev}"
+
 sudo cfdisk "$dev"
 guix shell e2fsprogs -- sudo resize2fs "$part"
 guix shell e2fsck-static -- sudo -E e2fsck "$part"
