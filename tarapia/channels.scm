f5987719a0fd320053b58cb024f678c79d37b086
Giacomo Leidi <goodoldpaul@autistici.org>
2023-10-06 23:51:09 +0200

    tarapia: Try and follow https://issues.guix.gnu.org/65982

--- tarapia/channels.scm
+++ tarapia/channels.scm
@@ -5,7 +5,7 @@
        (name 'guix)
        (url "https://git.savannah.gnu.org/git/guix.git")
        (commit
-        "fc3a53525ab3dcaf7c22eec8d62294017f9760fe")
+        "d6a53849935f8584e1df57faa79c18c23fbb2aa1")
        (introduction
         (make-channel-introduction
          "afb9f2752315f131e4ddd44eba02eed403365085"
--- tarapia/guix_pinebook_latest.sh
+++ tarapia/guix_pinebook_latest.sh
@@ -3,7 +3,7 @@ set -ex
 here="$(dirname "$0")"
 dev="/dev/nvme0n1"
 part="${dev}p1"
-image="$(guix time-machine -C "${here}/channels.scm" -- system image --image-type=pinebook-pro-raw -e '(@ (gnu system images pinebook-pro) pinebook-pro-barebones-os)' --system=aarch64-linux)"
+image="$(guix time-machine -C "${here}/channels.scm" -- system image -e '(@ (gnu system images pinebook-pro) pinebook-pro-barebones-raw-image)' --system=aarch64-linux)"
 
 
 "${here}/pbp-flash.sh" "${image}" "${dev}"
