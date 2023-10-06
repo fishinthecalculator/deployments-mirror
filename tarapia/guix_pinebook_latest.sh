7da03ef4d42b1dbfca6a5c0aa227d43fcb045307
Giacomo Leidi <goodoldpaul@autistici.org>
2023-10-07 00:37:44 +0200

    tarapia: Minor change.

--- tarapia/guix_pinebook_latest.sh
+++ tarapia/guix_pinebook_latest.sh
@@ -3,7 +3,7 @@ set -ex
 here="$(dirname "$0")"
 dev="/dev/nvme0n1"
 part="${dev}p1"
-image="$(guix time-machine -C "${here}/channels.scm" -- system image -e '(@ (gnu system images pinebook-pro) pinebook-pro-barebones-raw-image)' --system=aarch64-linux)"
+image="$(guix time-machine --commit=d6a53849935f8584e1df57faa79c18c23fbb2aa1 -- system image -e '(@ (gnu system images pinebook-pro) pinebook-pro-barebones-raw-image)' --system=aarch64-linux)"
 
 
 "${here}/pbp-flash.sh" "${image}" "${dev}"
