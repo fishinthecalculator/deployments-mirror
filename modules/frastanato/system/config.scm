42194261bf88edc7bf6a7c4ad6ee5d20ca9c7f18
Giacomo Leidi <goodoldpaul@autistici.org>
2024-01-29 21:12:00 +0100

    frastanato: update

--- modules/frastanato/system/config.scm
+++ modules/frastanato/system/config.scm
@@ -53,7 +53,10 @@
     (firmware (cons* realtek-firmware atheros-firmware %base-firmware))
 
     ;; The list of user accounts ('root' is implicit).
-    (users (cons* paul-user
+    (users (cons* (user-account
+                   (inherit paul-user)
+                   (comment "Tino il Cotechino")
+                   (supplementary-groups '("wheel" "netdev" "audio" "video" "docker")))
                   (user-account
                    (name "deploy")
                    (comment "Guix deploy user")
@@ -113,62 +116,62 @@
                              "ripgrep"))
                       %base-packages))
 
-  ;; Below is the list of system services.  To search for available
-  ;; services, run 'guix system search KEYWORD' in a terminal.
-  (services
-   (append (list
-            (service network-manager-service-type)
-            (service wpa-supplicant-service-type)
+    ;; Below is the list of system services.  To search for available
+    ;; services, run 'guix system search KEYWORD' in a terminal.
+    (services
+     (append (list
+              (service network-manager-service-type)
+              (service wpa-supplicant-service-type)
 
-            (deployments-unattended-upgrades host-name
-                                             #:expiration-days 30)
+              (deployments-unattended-upgrades host-name
+                                               #:expiration-days 30)
 
-            (service qemu-binfmt-service-type
-                     (qemu-binfmt-configuration (platforms (lookup-qemu-platforms
-                                                            "arm"
-                                                            "aarch64")))))
+              (service qemu-binfmt-service-type
+                       (qemu-binfmt-configuration (platforms (lookup-qemu-platforms
+                                                              "arm"
+                                                              "aarch64")))))
 
 
-           ;; This is the default list of services we
-           ;; are appending to.
-           (modify-services %small-guix-server-services
-             (delete dhcp-client-service-type)
-             (openssh-service-type ssh-config =>
-                                   (openssh-configuration (inherit ssh-config)
-                                                          (authorized-keys (append
-                                                                            (openssh-configuration-authorized-keys ssh-config)
-                                                                            authorized-ssh-keys))))
-             (guix-service-type guix-config =>
-                                (guix-configuration (inherit guix-config)
-                                                    (authorized-keys (append
-                                                                      (guix-configuration-authorized-keys guix-config)
-                                                                      authorized-guix-keys)))))))
+             ;; This is the default list of services we
+             ;; are appending to.
+             (modify-services %small-guix-server-services
+               (delete dhcp-client-service-type)
+               (openssh-service-type ssh-config =>
+                                     (openssh-configuration (inherit ssh-config)
+                                                            (authorized-keys (append
+                                                                              (openssh-configuration-authorized-keys ssh-config)
+                                                                              authorized-ssh-keys))))
+               (guix-service-type guix-config =>
+                                  (guix-configuration (inherit guix-config)
+                                                      (authorized-keys (append
+                                                                        (guix-configuration-authorized-keys guix-config)
+                                                                        authorized-guix-keys)))))))
 
-  (bootloader (bootloader-configuration
-               (bootloader grub-efi-bootloader)
-               (targets (list "/boot/efi"))
-               (keyboard-layout keyboard-layout)))
+    (bootloader (bootloader-configuration
+                 (bootloader grub-efi-bootloader)
+                 (targets (list "/boot/efi"))
+                 (keyboard-layout keyboard-layout)))
 
-  ;; The list of file systems that get "mounted".  The unique
-  ;; file system identifiers there ("UUIDs") can be obtained
-  ;; by running 'blkid' in a terminal.
-  (file-systems (cons* (file-system
-                         (mount-point "/")
-                         (device (uuid
-                                  "4d3ff686-809a-454d-8744-17f4ecd2adab"
-                                  'btrfs))
-                         (type "btrfs"))
-                       (file-system
-                         (mount-point "/boot/efi")
-                         (device (uuid "EB17-1A0F"
-                                       'fat32))
-                         (type "vfat")) %base-file-systems))
+    ;; The list of file systems that get "mounted".  The unique
+    ;; file system identifiers there ("UUIDs") can be obtained
+    ;; by running 'blkid' in a terminal.
+    (file-systems (cons* (file-system
+                           (mount-point "/")
+                           (device (uuid
+                                    "4d3ff686-809a-454d-8744-17f4ecd2adab"
+                                    'btrfs))
+                           (type "btrfs"))
+                         (file-system
+                           (mount-point "/boot/efi")
+                           (device (uuid "EB17-1A0F"
+                                         'fat32))
+                           (type "vfat")) %base-file-systems))
 
-  (swap-devices
-   (list
-    (swap-space
-     ;; See https://wiki.archlinux.org/title/Btrfs#Swap_file
-     ;; for swapfile on Btrfs
-     (target "/swap/swapfile")
-     (dependencies (filter (file-system-mount-point-predicate "/")
-                           file-systems)))))))
+    (swap-devices
+     (list
+      (swap-space
+       ;; See https://wiki.archlinux.org/title/Btrfs#Swap_file
+       ;; for swapfile on Btrfs
+       (target "/swap/swapfile")
+       (dependencies (filter (file-system-mount-point-predicate "/")
+                             file-systems)))))))
