7ec21b8f5fd9c953d7155e49992e7d6acb70523c
Giacomo Leidi <goodoldpaul@autistici.org>
2024-05-07 23:48:08 +0200

    fix typo

--- modules/common/services/server.scm
+++ modules/common/services/server.scm
@@ -28,7 +28,7 @@
                 (service ntp-service-type)
                 (service openssh-service-type
                          (openssh-configuration
-                          (root-login? #f)
+                          (permit-root-login #f)
                           (password-authentication? #f)
                           (x11-forwarding? #f)))
 
