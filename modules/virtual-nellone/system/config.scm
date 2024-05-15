5b765fc0cc74d55bc02e701c7ba26cd2556d75fd
Giacomo Leidi <goodoldpaul@autistici.org>
2024-05-15 21:19:39 +0200

    virtual-nellone: Update nginx config.

--- modules/virtual-nellone/system/config.scm
+++ modules/virtual-nellone/system/config.scm
@@ -141,7 +141,20 @@
                                  (list
                                   (nginx-location-configuration
                                    (uri "/")
-                                   (body (list (string-append "proxy_pass http://localhost:" %bonfire-port ";")))))))))))
+                                   (body (string-join (list (string-append "proxy_pass http://localhost:" %bonfire-port)
+                                                            ;; Taken from https://www.nginx.com/resources/wiki/start/topics/examples/full/
+                                                            ;; Those settings are used when proxies are involved
+                                                            "proxy_redirect          off"
+                                                            "proxy_set_header        Host $host"
+                                                            "proxy_set_header        X-Real-IP $remote_addr"
+                                                            "proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for"
+                                                            "proxy_http_version      1.1"
+                                                            "proxy_cache_bypass      $http_upgrade"
+                                                            "proxy_set_header        Upgrade $http_upgrade"
+                                                            "proxy_set_header        Connection \"upgrade\""
+                                                            "proxy_set_header        X-Forwarded-Proto $scheme"
+                                                            "proxy_set_header        X-Forwarded-Host  $host;")
+                                                      ";\n"))))))))))
 
               ;; Monitoring
               (service prometheus-node-exporter-service-type
