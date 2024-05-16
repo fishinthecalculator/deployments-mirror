6edf6648f04da8dd00ee81e163cd1225bfce6d5b
Giacomo Leidi <goodoldpaul@autistici.org>
2024-05-16 18:44:34 +0200

    virtual-nellone: Typo.

--- modules/virtual-nellone/system/config.scm
+++ modules/virtual-nellone/system/config.scm
@@ -29,7 +29,7 @@
   (let ((paul (user-account-name paul-user)))
     ;; List of authorized SSH keys.
     `((,paul ,paul-ssh-key)
-      (,paul ,gleidi-suse-key)
+      (,paul ,gleidi-suse-ssh-key)
       ("deploy" ,paul-ssh-key))))
 
 (define authorized-guix-keys
