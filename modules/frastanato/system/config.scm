e070d59351ca4507e316a912200e9214700a7b5a
Giacomo Leidi <goodoldpaul@autistici.org>
2024-09-07 04:19:25 +0200

    frastanato: cuirass: Add deployments.

--- modules/frastanato/system/config.scm
+++ modules/frastanato/system/config.scm
@@ -74,6 +74,21 @@
 (define %cuirass-specs
   #~(list
      (specification
+      (name "deployments")
+      (build '(channels deployments))
+      (channels
+       (cons (channel
+              (name 'deployments)
+              (url "https://gitlab.com/orang3/guix-deployments")
+              (branch "main")
+              ;; Enable signature verification:
+              (introduction
+               (make-channel-introduction
+                "9d101a2b1f38571e75e7d256bbc8d754177d11f3"
+                (openpgp-fingerprint
+                 "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+             %default-channels)))
+     (specification
       (name "mobilizon-reshare")
       (period #$%cuirass-period)
       (build '(packages
