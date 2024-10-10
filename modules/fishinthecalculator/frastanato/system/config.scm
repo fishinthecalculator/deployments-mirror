7ca13937b7d2fd38582c771af45ac797fb43407e
Giacomo Leidi <goodoldpaul@autistici.org>
2024-10-10 22:56:46 +0200

    frastanato: cuirass: Duplicate deployments job.

--- modules/fishinthecalculator/frastanato/system/config.scm
+++ modules/fishinthecalculator/frastanato/system/config.scm
@@ -89,8 +89,23 @@
                  "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
              %default-channels)))
      (specification
-      (name "deployments")
+      (name "deployments-ci")
       (build '(custom (fishinthecalculator ci)))
+      (channels
+       (cons (channel
+              (name 'deployments)
+              (url "https://gitlab.com/orang3/guix-deployments")
+              (branch "main")
+              ;; Enable signature verification:
+              (introduction
+               (make-channel-introduction
+                "9d101a2b1f38571e75e7d256bbc8d754177d11f3"
+                (openpgp-fingerprint
+                 "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+             %default-channels)))
+     (specification
+      (name "deployments-images")
+      (build '(images))
       (channels
        (cons (channel
               (name 'deployments)
