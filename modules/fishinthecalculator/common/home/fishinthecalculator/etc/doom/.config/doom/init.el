4e3e1cc523595194ab5d73ed5faec9c761b5909e
Giacomo Leidi <goodoldpaul@autistici.org>
2025-01-13 21:12:27 +0100

    Update doom defaults.

--- modules/fishinthecalculator/common/home/fishinthecalculator/etc/doom/.config/doom/init.el
+++ modules/fishinthecalculator/common/home/fishinthecalculator/etc/doom/.config/doom/init.el
@@ -69,6 +69,7 @@
        :emacs
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
+       ;;eww             ; the internet is gross
        ;;ibuffer         ; interactive buffer management
        (undo +tree)      ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
@@ -154,6 +155,7 @@
        (org +pandoc)       ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
+       ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
