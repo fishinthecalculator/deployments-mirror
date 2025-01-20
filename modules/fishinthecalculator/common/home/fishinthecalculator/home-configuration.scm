1ba9f5dc711b7279b32b36c2aa5aaedc199d8207
Giacomo Leidi <goodoldpaul@autistici.org>
2025-01-20 18:34:08 +0100

    Update backup content.

--- modules/fishinthecalculator/common/home/fishinthecalculator/home-configuration.scm
+++ modules/fishinthecalculator/common/home/fishinthecalculator/home-configuration.scm
@@ -251,7 +251,9 @@ without waiting for the scheduled time."))
    (password-file "/run/user/1001/secrets/restic")
    ;; Every day at 10
    (schedule "0 10 * * *")
-   (files '("/home/paul/code/personal"))
+   (files '("/home/paul/code/personal"
+            "/home/paul/.config/guix/channels.scm"
+            "/home/paul/.guix-manifests"))
    (verbose? #t)))
 
 (define thinkpad-paul.yaml
