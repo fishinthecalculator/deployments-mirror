56c09124205beb5fe3962f35b88758ce6a2ccb20
Giacomo Leidi <goodoldpaul@autistici.org>
2025-02-08 01:47:22 +0100

    Add test live image.

--- modules/fishinthecalculator/ci.scm
+++ modules/fishinthecalculator/ci.scm
@@ -1,9 +1,10 @@
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
-;;; Copyright © 2024 Giacomo Leidi <goodoldpaul@autistici.org>
+;;; Copyright © 2024, 2025 Giacomo Leidi <goodoldpaul@autistici.org>
 
 (define-module (fishinthecalculator ci)
   #:use-module (gnu ci)
   #:use-module (gnu system image)
+  #:use-module (fishinthecalculator common system image)
   #:use-module (fishinthecalculator frastanato system image)
   #:use-module (fishinthecalculator prematurata system image)
   #:use-module (srfi srfi-1)
@@ -16,6 +17,10 @@
   (append-map
    (lambda (system)
      (list
+      (image->job store
+                  installation-os-deployments-image
+                  #:name "installation-os-deployments-image"
+                  #:system system)
       (image->job store
                   frastanato-system-tarball
                   #:name "frastanato-system-tarball"
---
+++ modules/fishinthecalculator/common/system/image.scm
@@ -0,0 +1,27 @@
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+;;; Copyright © 2025 Giacomo Leidi <goodoldpaul@autistici.org>
+
+;; Generate a bootable image (e.g. for USB sticks, etc.) with:
+;; $ guix system image -L `pwd`/modules modules/common/system/image.scm
+
+(define-module (fishinthecalculator common system image)
+  #:use-module (gnu image)
+  #:use-module (gnu system image)
+  #:use-module (fishinthecalculator common system install)
+  #:export (installation-os-deployments-image))
+
+(define installation-os-deployments-image
+  (image-with-os iso9660-image
+                 (operating-system (inherit installation-os-deployments)
+                                   (file-systems
+                                    (list (file-system
+                                            (mount-point "/")
+                                            (device (file-system-label "Guix_image"))
+                                            (type "ext4"))
+                                          (file-system
+                                            (mount-point "/tmp")
+                                            (device "none")
+                                            (type "tmpfs")
+                                            (check? #f)))))))
+
+installation-os-deployments-image
