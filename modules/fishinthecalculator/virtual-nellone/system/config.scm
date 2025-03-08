17b2eae9fe03a9f23a5bccf25bd256fe4a050c46
Giacomo Leidi <goodoldpaul@autistici.org>
2025-03-08 01:40:09 +0100

    Refresh virtual-nellone.

--- modules/fishinthecalculator/virtual-nellone/system/config.scm
+++ modules/fishinthecalculator/virtual-nellone/system/config.scm
@@ -1,8 +1,9 @@
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
-;;; Copyright © 2024 Giacomo Leidi <goodoldpaul@autistici.org>
+;;; Copyright © 2024, 2025 Giacomo Leidi <goodoldpaul@autistici.org>
 
 (define-module (fishinthecalculator virtual-nellone system config)
   #:use-module (gnu)
+  #:use-module (gnu system accounts)
   #:use-module (gnu packages databases)      ;for postgresql-13
   #:use-module (gnu packages geo)            ;for postgis
   #:use-module (gnu services certbot)        ;for certbot-service-type
@@ -29,22 +30,21 @@
   (let ((paul (user-account-name paul-user)))
     ;; List of authorized SSH keys.
     `((,paul ,paul-ssh-key)
-      (,paul ,gleidi-suse-ssh-key)
       ("deploy" ,paul-ssh-key))))
 
 (define authorized-guix-keys
   ;; List of authorized 'guix archive' keys.
   (list prematurata-guix-key))
 
-(define %bonfire-port "4000")
-(define %bonfire-domain "bonfire.fishinthecalculator.me")
-(define %grafana-port "3000")
-(define %meilisearch-port "7700")
-(define %postgresql-port 5432)
-(define %prometheus-port "9000")
-(define %prometheus-metrics-port "9090")
-(define %node-exporter-port "9100")
+(define %forgejo-port "3000")
+(define %forgejo-domain "forgejo.fishinthecalculator.me")
 
+(define subgids
+  (list (subid-range (name (user-account-name paul-user)))))
+(define subuids
+  (list (subid-range (name (user-account-name paul-user)))))
+(define %common-server-services
+  (common-server-services subuids subgids))
 (define virtual-nellone-system
   (operating-system
     (locale "en_US.utf8")
@@ -56,7 +56,7 @@
     (users (cons* (user-account
                    (inherit paul-user)
                    (comment "Tino il Cotechino")
-                   (supplementary-groups '("wheel" "netdev" "audio" "video" "docker")))
+                   (supplementary-groups '("wheel" "netdev" "audio" "video" "cgroup")))
                   (user-account
                    (name "deploy")
                    (comment "Guix deploy user")
@@ -98,16 +98,11 @@
                              "tmux"
                              "vim"
 
-                             ;; DB Administration
-                             "postgresql@14" ;for psql
-
                              ;; Network administration
                              "bind"
                              "bind:utils"
                              "tcpdump"
                              "net-tools"
-
-                             "restic"
 
                              "efibootmgr"
 
@@ -121,156 +116,24 @@
     ;; services, run 'guix system search KEYWORD' in a terminal.
     (services
      (append (list
-              (service kernel-module-loader-service-type
-                       ;; Required for Traefik.
-                       '("br_netfilter"))
               (service certbot-service-type
                        (certbot-configuration
                         (email "goodoldpaul@autistici.org")
                         (certificates
                          (list
                           (certificate-configuration
-                           (domains (list %bonfire-domain)))))))
-
-              ;; (service nginx-service-type
-              ;;          (nginx-configuration
-              ;;           (shepherd-requirement
-              ;;            '(docker-bonfire))
-              ;;           (server-blocks
-              ;;            (list (nginx-server-configuration
-              ;;                   (server-name (list %bonfire-domain))
-              ;;                   (listen '("443 ssl"))
-              ;;                   (ssl-certificate (string-append "/etc/certs/" %bonfire-domain "/fullchain.pem"))
-              ;;                   (ssl-certificate-key (string-append "/etc/certs/" %bonfire-domain "/privkey.pem"))
-              ;;                   (locations
-              ;;                    (list
-              ;;                     (nginx-location-configuration
-              ;;                      (uri "/")
-              ;;                      (body (list (string-append "proxy_pass http://localhost:" %bonfire-port ";")
-              ;;                                  ;; Taken from https://www.nginx.com/resources/wiki/start/topics/examples/full/
-              ;;                                  ;; Those settings are used when proxies are involved
-              ;;                                  "proxy_redirect          off;"
-              ;;                                  "proxy_set_header        Host $host;"
-              ;;                                  "proxy_set_header        X-Real-IP $remote_addr;"
-              ;;                                  "proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;"
-              ;;                                  "proxy_http_version      1.1;"
-              ;;                                  "proxy_cache_bypass      $http_upgrade;"
-              ;;                                  "proxy_set_header        Upgrade $http_upgrade;"
-              ;;                                  "proxy_set_header        Connection \"upgrade\";"
-              ;;                                  "proxy_set_header        X-Forwarded-Proto $scheme;"
-              ;;                                  "proxy_set_header        X-Forwarded-Host  $host;"))))))))))
-
-              ;; Monitoring
-              (service prometheus-node-exporter-service-type
-                       (prometheus-node-exporter-configuration
-                        (web-listen-address
-                         (string-append ":" %node-exporter-port))))
-
-              (service oci-prometheus-service-type
-                       (oci-prometheus-configuration
-                        (image "prom/prometheus:v2.45.0")
-                        (network "host")
-                        (port %prometheus-port)
-                        (metrics-port %prometheus-metrics-port)
-                        (record
-                         (prometheus-configuration
-                          (global
-                           (prometheus-global-configuration
-                            (scrape-interval "30s")
-                            (scrape-timeout "12s")))
-                          (scrape-configs
-                           (list
-                            (prometheus-scrape-configuration
-                             (job-name "prometheus")
-                             (static-configs
-                              (list (prometheus-static-configuration
-                                     (targets
-                                      (list
-                                       (string-append "localhost:" %prometheus-metrics-port)))))))
-                            (prometheus-scrape-configuration
-                             (job-name "node")
-                             (static-configs
-                              (list (prometheus-static-configuration
-                                     (targets
-                                      (list (string-append "localhost:" %node-exporter-port)))))))))))))
-
-              (service oci-grafana-service-type
-                       (oci-grafana-configuration
-                        (network "host")
-                        (port %grafana-port)))
-
-              ;; Bonfire
-              ;; (service oci-bonfire-service-type
-              ;;          (oci-bonfire-configuration
-              ;;           (image "bonfirenetworks/bonfire:0.9.10-beta.70-classic-amd64")
-              ;;           (log-file "/var/log/bonfire.log")
-              ;;           (configuration
-              ;;            (bonfire-configuration
-              ;;             (hostname %bonfire-domain)
-              ;;             (port %bonfire-port)
-              ;;             (public-port "443")
-              ;;             (postgres-user "bonfire")
-              ;;             (postgres-db "bonfire")
-              ;;             (mail-domain %bonfire-domain)
-              ;;             (mail-from (string-append "friendlyadmin@" %bonfire-domain))))
-              ;;           (network "host")
-              ;;           (auto-start? #t)
-              ;;           (requirement
-              ;;            '(sops-secrets postgres-roles docker-meilisearch))
-              ;;           (extra-variables
-              ;;            `("INVITE_ONLY=true"
-              ;;              ("MAIL_BACKEND" . "sendgrid")
-              ;;              ("SERVER_PORT" . ,%bonfire-port)
-              ;;              ("SEARCH_MEILI_INSTANCE" . ,(string-append "http://localhost:" %meilisearch-port))))
-              ;;           (meili-master-key
-              ;;            meilisearch-key-secret)
-              ;;           (postgres-password
-              ;;            postgres-password-secret)
-              ;;           (mail-password
-              ;;            mail-password-secret)
-              ;;           (secret-key-base
-              ;;            secret-key-base-secret)
-              ;;           (signing-salt
-              ;;            signing-salt-secret)
-              ;;           (encryption-salt
-              ;;            encryption-salt-secret)))
-
-              ;; (service oci-meilisearch-service-type
-              ;;          (oci-meilisearch-configuration
-              ;;           (network "host")
-              ;;           (port %meilisearch-port)
-              ;;           (master-key
-              ;;            meilisearch-key-secret)))
-
-              ;; (service postgresql-role-service-type
-              ;;          (postgresql-role-configuration
-              ;;           (roles
-              ;;            (list (postgresql-role
-              ;;                   (name "bonfire")
-              ;;                   (create-database? #t))))))
-
-              ;; (service postgresql-service-type
-              ;;          (postgresql-configuration
-              ;;           (postgresql postgresql-14)
-              ;;           (extension-packages (list postgis))
-              ;;           (port %postgresql-port)))
+                           (domains (list %forgejo-domain)))))))
 
               (service sops-secrets-service-type
                        (sops-service-configuration
-                        (config sops.yaml)
-                        (secrets
-                         (list postgres-password-secret
-                               mail-password-secret
-                               secret-key-base-secret
-                               signing-salt-secret
-                               encryption-salt-secret))))
+                        (config sops.yaml)))
 
               (deployments-unattended-upgrades host-name
                                                #:expiration-days 30))
 
              ;; This is the default list of services we
              ;; are appending to.
-             (modify-services (common-server-services '() '()) ;; FIXME: set subid ranges
+             (modify-services %common-server-services
                (openssh-service-type ssh-config =>
                                      (openssh-configuration (inherit ssh-config)
                                                             (authorized-keys
@@ -287,38 +150,18 @@
     (bootloader (bootloader-configuration
                  (bootloader grub-bootloader)
                  (targets (list "/dev/vda"))
-                 (keyboard-layout keyboard-layout)
-                 ;; guix shell cpio -- sh -c 'echo /crypto.key | cpio -oH newc > /crypto.cpio'
-                 ;; chmod 0000 /crypto.cpio
-                 ;; Load the initrd with a key file
-                 (extra-initrd "/crypto.cpio")))
-    (mapped-devices (list (mapped-device
-                           (source (uuid
-                                    "b8482f5a-d64a-4501-a52d-f2439f9f786a"))
-                           (target "cryptroot")
-                           (type (luks-device-mapping-with-options
-                                  ;; All the following must be run as root
-                                  ;; DEST="/crypto.key"
-                                  ;; dd bs=512 count=4 if=/dev/random of=$DEST iflag=fullblock
-                                  ;; guix shell openssl -- openssl genrsa -out $DEST 4096
-                                  ;; chmod -v 0400 $DEST
-                                  ;; chown root:root $DEST
-                                  #:key-file "/crypto.key")))))
+                 (keyboard-layout keyboard-layout)))
+
+    (swap-devices (list (swap-space
+                          (target (uuid
+                                   "97f21590-e3ae-43c6-b637-648a5aa3bfde")))))
 
     ;; The list of file systems that get "mounted".  The unique
     ;; file system identifiers there ("UUIDs") can be obtained
     ;; by running 'blkid' in a terminal.
     (file-systems (cons* (file-system
                            (mount-point "/")
-                           (device "/dev/mapper/cryptroot")
-                           (type "ext4")
-                           (dependencies mapped-devices)) %base-file-systems))
-
-    (swap-devices
-     (list
-      (swap-space
-       ;; See https://www.cyberciti.biz/faq/linux-add-a-swap-file-howto/
-       ;; for swapfile on ext4
-       (target "/swapfile")
-       (dependencies (filter (file-system-mount-point-predicate "/")
-                             file-systems)))))))
+                           (device (uuid
+                                    "373ba527-a118-4df9-8759-50918ace4703"
+                                    'ext4))
+                           (type "ext4")) %base-file-systems))))
