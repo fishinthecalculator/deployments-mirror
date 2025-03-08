f3b31c7c0ec8a9e53127dd7fb764f65db32534bc
Giacomo Leidi <goodoldpaul@autistici.org>
2025-03-08 21:24:29 +0100

    Add nginx

--- modules/fishinthecalculator/virtual-nellone/system/config.scm
+++ modules/fishinthecalculator/virtual-nellone/system/config.scm
@@ -142,9 +142,38 @@
                         (runtime 'podman)
                         (verbose? #t)))
 
+              (service nginx-service-type
+                       (nginx-configuration
+                        ;; Wait for forgejo to start
+                        (shepherd-requirement
+                         '(podman-forgejo))
+                        (server-blocks
+                         (list (nginx-server-configuration
+                                (server-name (list %forgejo-domain))
+                                (listen '("443 ssl"))
+                                (ssl-certificate (string-append "/etc/certs/" %forgejo-domain "/fullchain.pem"))
+                                (ssl-certificate-key (string-append "/etc/certs/" %forgejo-domain "/privkey.pem"))
+                                (locations
+                                 (list
+                                  (nginx-location-configuration
+                                   (uri "/")
+                                   (body (list (string-append "proxy_pass http://localhost:" %forgejo-port ";")
+                                               ;; Taken from https://www.nginx.com/resources/wiki/start/topics/examples/full/
+                                               ;; Those settings are used when proxies are involved
+                                               "proxy_redirect          off;"
+                                               "proxy_set_header        Host $host;"
+                                               "proxy_set_header        X-Real-IP $remote_addr;"
+                                               "proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;"
+                                               "proxy_http_version      1.1;"
+                                               "proxy_cache_bypass      $http_upgrade;"
+                                               "proxy_set_header        Upgrade $http_upgrade;"
+                                               "proxy_set_header        Connection \"upgrade\";"
+                                               "proxy_set_header        X-Forwarded-Proto $scheme;"
+                                               "proxy_set_header        X-Forwarded-Host  $host;"))))))))))
+
               ;; Misc
               (service common-unload-service-type
-                       '("podman-forgejo"))
+                       '("nginx" "podman-forgejo"))
 
               (deployments-unattended-upgrades host-name
                                                #:expiration-days 30))
