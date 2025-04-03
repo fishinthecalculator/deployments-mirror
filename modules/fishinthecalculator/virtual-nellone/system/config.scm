93617d3ac380633d13f7d93a5b8377e99a9bef82
Giacomo Leidi <goodoldpaul@autistici.org>
2025-04-03 23:28:09 +0200

    virtual-nellone: Filter traffic on port 80 .

--- modules/fishinthecalculator/virtual-nellone/system/config.scm
+++ modules/fishinthecalculator/virtual-nellone/system/config.scm
@@ -153,7 +153,6 @@
 :OUTPUT ACCEPT
 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 -A INPUT -p tcp --dport 22 -j ACCEPT
--A INPUT -p tcp --dport 80 -j ACCEPT
 -A INPUT -p tcp --dport 443 -j ACCEPT
 -A INPUT -i lo -j ACCEPT
 -A INPUT -j REJECT --reject-with icmp-port-unreachable
@@ -165,7 +164,6 @@ COMMIT
 :OUTPUT ACCEPT
 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 -A INPUT -p tcp --dport 22 -j ACCEPT
--A INPUT -p tcp --dport 80 -j ACCEPT
 -A INPUT -p tcp --dport 443 -j ACCEPT
 -A INPUT -i lo -j ACCEPT
 -A INPUT -j REJECT --reject-with icmp6-port-unreachable
