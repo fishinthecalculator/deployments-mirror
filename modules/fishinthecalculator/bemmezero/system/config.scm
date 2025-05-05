b075de3fe1725254b3fbc4a5c548b16661e1be12
Giacomo Leidi <goodoldpaul@autistici.org>
2025-05-05 17:30:06 +0200

    bemmezero: Drop mailjet challenge.

--- modules/fishinthecalculator/bemmezero/system/config.scm
+++ modules/fishinthecalculator/bemmezero/system/config.scm
@@ -206,15 +206,6 @@
                          '(docker-bonfire))
                         (server-blocks
                          (list
-                          (nginx-server-configuration
-                           (server-name (list %bonfire-domain))
-                           (listen '("80"))
-                           (locations
-                            (list
-                             (nginx-location-configuration
-                              (uri "/c4675f4d88b774d8f032d4c763e00631.txt")
-                              (body
-                               (list (string-append "alias /tmp/c4675f4d88b774d8f032d4c763e00631.txt;")))))))
                           (bonfire-nginx-server %bonfire-domain %bonfire-port %bonfire-upload-data-directory)))))
 
               ;; Misc
@@ -236,6 +227,7 @@
 :OUTPUT ACCEPT
 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 -A INPUT -p tcp --dport 22 -j ACCEPT
+-A INPUT -p tcp --dport 80 -j ACCEPT
 -A INPUT -p tcp --dport 443 -j ACCEPT
 -A INPUT -i lo -j ACCEPT
 -A INPUT -j REJECT --reject-with icmp-port-unreachable
@@ -247,6 +239,7 @@ COMMIT
 :OUTPUT ACCEPT
 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 -A INPUT -p tcp --dport 22 -j ACCEPT
+-A INPUT -p tcp --dport 80 -j ACCEPT
 -A INPUT -p tcp --dport 443 -j ACCEPT
 -A INPUT -i lo -j ACCEPT
 -A INPUT -j REJECT --reject-with icmp6-port-unreachable
