e4964b46f191b6bc5d88205cfaf7653fb8750bd7
Giacomo Leidi <goodoldpaul@autistici.org>
2025-06-01 00:54:42 +0200

    bemmezero: Verbose containers.

--- modules/fishinthecalculator/bemmezero/system/config.scm
+++ modules/fishinthecalculator/bemmezero/system/config.scm
@@ -182,6 +182,10 @@
                         (encryption-salt
                          encryption-salt-secret)))
 
+              (service oci-service-type
+                       (oci-configuration
+                        (verbose? #t)))
+
               (simple-service 'oci-provisioning
                               oci-service-type
                               (oci-extension
