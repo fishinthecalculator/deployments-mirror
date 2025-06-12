9f407187b46b7e18f922149b22113a436e6a60cc
Giacomo Leidi <goodoldpaul@autistici.org>
2025-06-12 21:42:23 +0200

    prematurata: Drop charlie.

--- modules/fishinthecalculator/prematurata/system/config.scm
+++ modules/fishinthecalculator/prematurata/system/config.scm
@@ -159,14 +159,7 @@ without waiting for the scheduled time."))
 
     (host-name "prematurata")
 
-    (users (cons* (user-account
-                   (name "charlie")
-                   (group "users")
-
-                   ;; Specify a SHA-512-hashed initial password.
-                   (password (crypt "test" "$6$abc"))
-                   (home-directory "/home/charlie"))
-                  paul-user
+    (users (cons* paul-user
                   %base-user-accounts))
 
     ;; Operating system with encrypted boot partition
