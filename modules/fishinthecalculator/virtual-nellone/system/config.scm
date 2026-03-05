6db504f8bb2c52eddebce5a53e6f45a49096f8e9
Giacomo Leidi <therewasa@fishinthecalculator.me>
2026-03-05 01:13:34 +0100

    virtual-nellone: Reformat

--- modules/fishinthecalculator/virtual-nellone/system/config.scm
+++ modules/fishinthecalculator/virtual-nellone/system/config.scm
@@ -229,25 +229,25 @@
                                bonfire-mail-private-key-secret
                                bonfire-secret-key-base-secret
                                bonfire-signing-salt-secret
-                               bonfire-encryption-salt-secret)))
+                               bonfire-encryption-salt-secret))))
 
-                   ;; Syncthing
-                   (service syncthing-service-type
-                            (syncthing-configuration
-                             (user paul-name)
-                             (config-file
-                              (syncthing-config-file
-                               (start-browser? #f)
-                               (folders
-                                (list
-                                 (syncthing-folder
-                                  (id "weapw-rjelk")
-                                  (label "secrets")
-                                  (path "~/.secrets")
-                                  (devices
-                                   (list (syncthing-device
-                                          (name "skibidi")
-                                          (id "UJYN5HQ-U5BN3QR-STOGFV3-YV4NDYY-PEUHV36-LDVVWXA-OXY4ZL3-ECCKKQM"))))))))))))
+              ;; Syncthing
+              (service syncthing-service-type
+                       (syncthing-configuration
+                        (user paul-name)
+                        (config-file
+                         (syncthing-config-file
+                          (start-browser? #f)
+                          (folders
+                           (list
+                            (syncthing-folder
+                             (id "weapw-rjelk")
+                             (label "secrets")
+                             (path "~/.secrets")
+                             (devices
+                              (list (syncthing-device
+                                     (name "skibidi")
+                                     (id "UJYN5HQ-U5BN3QR-STOGFV3-YV4NDYY-PEUHV36-LDVVWXA-OXY4ZL3-ECCKKQM")))))))))))
 
               ;; Backups
               (service restic-backup-service-type
